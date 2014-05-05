a5506c57b2d5705fe84e721e7ae19e133630424e
Matthew Fabb <mattf@thesecretlocation.com>
2014-05-05 16:47:56 -0400

    Working as a class
    Next up is to make the match string external.

--- js/speech_commands.js
+++ js/speech_commands.js
@@ -1,37 +1,30 @@
-var match_string = "next";
-var final_transcript = '';
-var number_of_matches = 0;
-var recognizing = false;
-var ignore_onend;
-var start_timestamp;
-var speechRecognition;
-var eventResults;
-var latest_words = "";
-var previousMatch = -1;
-
 var speechCommands = {
 	_this: null,
 	match_string: "next page",
-	speechRecognition:null,
+	speechRecognition: null,
+	final_transcript: "",
+	number_of_matches: 0,
+	recognizing: false,
+	ignore_onend: false,
+	previousMatch: -1,
 	
 	init:function() {
 		_this = this;
 		
 		// checks to see if SpeechRecognition otherwise asks the user to upgrade
 		// note only webkitSpeechRecognition exists right now, this is just future proofing it
-		if (('webkitSpeechRecognition' in window) || ('mozSpeechRecognition' in window) || ('msSpeechRecognition' in window) || ('SpeechRecognition' in window)) {
+		if (('webkitSpeechRecognition' in window) || ('SpeechRecognition' in window)) {
+			_this.speechRecognition = new webkitSpeechRecognition();
+			_this.speechRecognition.continuous = true; // speech will continue after small pauses and will only stop after a very long pause
+			_this.speechRecognition.interimResults = true; // determines whether interim data is sent or if it is just final data
+		
 			_this.setSpeechRecEvents();
 		} else {
-			_this.browserNotSupporter();
+			_this.browserNotSupported();
 		}
 	},
 	
 	setSpeechRecEvents:function () {
-		_this.speechRecognition = new webkitSpeechRecognition();
-		
-		_this.speechRecognition.continuous = true;
-		_this.speechRecognition.interimResults = true;
-
 		_this.speechRecognition.onstart = _this.speechOnStartHandler;
 		_this.speechRecognition.onerror = _this.speechOnErrorHandler;
 		_this.speechRecognition.onend = _this.speechOnEndHandler;
@@ -41,24 +34,25 @@ var speechCommands = {
 	speechOnStartHandler:function(event) {
 		console.log("It has started!", Date.now());
           
-        recognizing = true;
+        _this.recognizing = true;
 	},
 	speechOnErrorHandler:function(event) {
 		if (event.error == 'no-speech') {
-            ignore_onend = true;
+            _this.ignore_onend = true;
         } else if (event.error == 'audio-capture') {
-            ignore_onend = true;
+            _this.ignore_onend = true;
         } else if (event.error == 'not-allowed') {
-            ignore_onend = true;
+            _this.ignore_onend = true;
         }
 		console.error("ERROR", event.error, Date.now());
 	},
 	speechOnEndHandler:function(event) {
-		recognizing = false;
-        if (ignore_onend) {
+		_this.recognizing = false;
+		
+        if (_this.ignore_onend) {
             return;
         }
-        if (!final_transcript) {
+        if (!_this.final_transcript) {
             return;
         }
         
@@ -66,55 +60,56 @@ var speechCommands = {
 	},
 	speechOnResultHandler:function(event) {
 		var interim_transcript = '';
-        if (typeof(event.results) == 'undefined') {
+		var eventResults = event.results;
+		
+        if (typeof(eventResults) == 'undefined') {
             //this.speechRecognition.onend = null;
             //this.speechRecognition.stop();
             console.log("No results from SpeechRecognitionEvent", Date.now());
             return;
         }
-        eventResults = event.results;
-        for (var i = event.resultIndex; i < event.results.length; ++i) {
-            if (event.results[i].isFinal) {
-                latest_words = event.results[i][0].transcript;
-                final_transcript += latest_words;
+		var latest_words = "";
+        for (var i = event.resultIndex; i < eventResults.length; ++i) {
+            if (eventResults[i].isFinal) {
+                latest_words = eventResults[i][0].transcript;
+                _this.final_transcript += latest_words;
             } else {
-                interim_transcript += event.results[i][0].transcript;
+                interim_transcript += eventResults[i][0].transcript;
             }            
-            console.log("event.results[" + i + "][0].transcript", event.results[i][0].transcript, Date.now());
+            console.log("eventResults[" + i + "][0].transcript", eventResults[i][0].transcript, Date.now());
         }
         
-        if ((latest_words.indexOf(match_string) > -1) && (previousMatch != event.results.length)) {
+        if ((latest_words.indexOf(_this.match_string) > -1) && (_this.previousMatch != eventResults.length)) {
             latest_words = "";
-            previousMatch = event.results.length;
-            console.warn("Match found!", event.results.length);
-            number_of_matches++;
+            _this.previousMatch = eventResults.length;
+            console.warn("Match found!", eventResults.length);
+            _this.number_of_matches++;
         }
         
         console.log("latest_words", latest_words);
         
-        //console.log("final_transcript", final_transcript, Date.now());
+        //console.log("_this.final_transcript", _this.final_transcript, Date.now());
         //console.log("interim_transcript", interim_transcript, Date.now());
-        final_transcript = capitalize(final_transcript);
-        console.log("transcript:", linebreak(final_transcript));
+        _this.final_transcript = capitalize(_this.final_transcript);
+        console.log("transcript:", linebreak(_this.final_transcript));
         console.log("interim:", linebreak(interim_transcript));
 	},
 	
-	browserNotSupporter:function() {
-		var msg = "This functionality is currently on available in Chrome 25+ desktop, Chrome 33+ for mobile";
-		console.error(msg);
+	browserNotSupported:function() {
+		console.error("This functionality is currently on available in Chrome 25+ desktop, Chrome 33+ for Android");
 	},
 	
 	startSpeechRecog:function() {
-		if (recognizing) {
+		if (_this.recognizing) {
 			_this.speechRecognition.stop();
 			return;
 		}
-		final_transcript = '';
+		_this.final_transcript = '';
 		_this.speechRecognition.lang = 'en-CA'; // English Canada
 		_this.speechRecognition.start();
-		ignore_onend = false;
+		_this.ignore_onend = false;
 	},	
-}
+};
 
 speechCommands.init();
 
